f60f012d64ce5725db23e7b33cdd617851b47b37
Salchy <leandrocg524@gmail.com>
2025-05-06 09:48:17 -0300

    Creacion del proyecto

---
+++ inventory/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("inventory")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("inventory")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("05fdb29a-a39b-44d1-979b-739046073228")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ inventory/frmMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace inventory
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnVerEmpresas = new System.Windows.Forms.Button();
+            this.btnVerStock = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnVerEmpresas
+            // 
+            this.btnVerEmpresas.Location = new System.Drawing.Point(145, 113);
+            this.btnVerEmpresas.Name = "btnVerEmpresas";
+            this.btnVerEmpresas.Size = new System.Drawing.Size(115, 25);
+            this.btnVerEmpresas.TabIndex = 0;
+            this.btnVerEmpresas.Text = "VER EMPRESAS";
+            this.btnVerEmpresas.UseVisualStyleBackColor = true;
+            // 
+            // btnVerStock
+            // 
+            this.btnVerStock.Location = new System.Drawing.Point(145, 165);
+            this.btnVerStock.Name = "btnVerStock";
+            this.btnVerStock.Size = new System.Drawing.Size(115, 25);
+            this.btnVerStock.TabIndex = 0;
+            this.btnVerStock.Text = "VER STOCK";
+            this.btnVerStock.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 411);
+            this.Controls.Add(this.btnVerStock);
+            this.Controls.Add(this.btnVerEmpresas);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control de inventario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerEmpresas;
+        private System.Windows.Forms.Button btnVerStock;
+    }
+}
+
